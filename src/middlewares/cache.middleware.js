d3f095daeb8d85ca84e1ae58e39ba3709f4d78a0
sassenagh <anna.alcaide.hijano@gmail.com>
2020-06-29 23:39:58 +0200

    swagger added

--- src/middlewares/cache.middleware.js
+++ src/middlewares/cache.middleware.js
@@ -1,20 +1,26 @@
-const mcache = require('memory-cache');
-const { CACHE_KEY } = require('../config');
+const mcache = require ('memory-cache');
+const {CACHE_KEY} = require ('../config');
 
 module.exports = function (duration) {
   return (req, res, next) => {
-    const key = CACHE_KEY + req.originUrl || req.url;
-    const cachedBody = mcache.get(key);
+    const {id, name} = req.query;
+    if (id) {
+      query = req.query.id;
+    } else {
+      query = req.query.name;
+    }
+    const key = CACHE_KEY + req.originUrl + query || req.url + query;
+    const cachedBody = mcache.get (key);
 
     if (cachedBody) {
-      return res.send(JSON.parse(cachedBody));
+      return res.send (JSON.parse (cachedBody));
     } else {
       res.sendResponse = res.send;
-      res.send = (body) => {
-        mcache.put(key, body, duration * 1000);
-        res.sendResponse(body);
+      res.send = body => {
+        mcache.put (key, body, duration * 1000);
+        res.sendResponse (body);
       };
-      next();
+      next ();
     }
   };
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,34 +1,34 @@
-const express = require ('express');
-const cors = require ('cors');
-const helmet = require ('helmet');
-const compression = require ('compression');
-require ('express-async-errors');
-const {NotFoundMiddleware, ErrorMiddleware} = require ('../middlewares');
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const compression = require('compression');
+require('express-async-errors');
+const { NotFoundMiddleware, ErrorMiddleware } = require('../middlewares');
+const swaggerUI = require('swagger-ui-express');
+const { SWAGGER_PATH } = require('../config');
+const swaggerDocument = require(SWAGGER_PATH);
 
-module.exports = function({
+module.exports = function ({
   HomeRoutes,
   ClientRoutes,
   PolicyRoutes,
   AuthRoutes,
 }) {
-  const router = express.Router ();
-  const apiRoutes = express.Router ();
+  const router = express.Router();
+  const apiRoutes = express.Router();
 
-  apiRoutes
-    .use (express.json ())
-    .use (cors ())
-    .use (helmet ())
-    .use (compression ());
+  apiRoutes.use(express.json()).use(cors()).use(helmet()).use(compression());
 
-  apiRoutes.use ('/home', HomeRoutes);
-  apiRoutes.use ('/clients', ClientRoutes);
-  apiRoutes.use ('/policies', PolicyRoutes);
-  apiRoutes.use ('/', AuthRoutes);
+  apiRoutes.use('/home', HomeRoutes);
+  apiRoutes.use('/clients', ClientRoutes);
+  apiRoutes.use('/policies', PolicyRoutes);
+  apiRoutes.use('/', AuthRoutes);
 
-  router.use ('/api', apiRoutes);
+  router.use('/api', apiRoutes);
+  router.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
-  router.use (NotFoundMiddleware);
-  router.use (ErrorMiddleware);
+  router.use(NotFoundMiddleware);
+  router.use(ErrorMiddleware);
 
   return router;
 };
